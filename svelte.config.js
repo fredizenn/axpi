76975bb72be5434db83945b2998bcbca7d6655c5
Frederick Akuffo <kofiakuffoboafo@gmail.com>
2025-10-29 19:30:56 +0000

    svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -6,9 +6,17 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({
-		fallback: 'index.html'
-	}) }
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html', // 👈 important
+			precompress: false
+		}),
+		prerender: {
+			handleHttpError: 'warn'
+		}
+	}
 };
 
 export default config;
